455b6d6b08194e3e1ec58d9bffa7738d1f4491b4
isaaccleopas <isaaccleopas@gmail.com>
2023-04-24 18:43:31 +0000

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
-                "task": t.get("title"),
+                "task": l.get("title"),
                 "completed": t.get("completed"),
                 "username": username
-            } for t in todos]}, jsonfile)
+            } for l in todos]}, jsonfile)
