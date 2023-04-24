3e5a8eb9e4fadf8165a149aada465e4cda8606ef
isaaccleopas <isaaccleopas@gmail.com>
2023-04-24 16:38:04 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    employee_id = sys.argv[1]
+    users = requests.get(base_url + 'users/{}'.format(employee_id)).json()
+    todo_list = requests.get(base_url + 'todos', params={'userId': employee_id}).json()
+
+    employee_name = users['name']
+    num_completed_tasks = [
+        todo_item.get('title') for todo_item in todo_list
+        if todo_item.get('userId') == int(employee_id) and todo_item.get('completed') is True
+    ]
+    total_num_tasks = len(todo_list)
+
+    print('Employee {} is done with tasks ({}/{}):'.format(
+        employee_name, len(num_completed_tasks), total_num_tasks))
+    [print('\t{}'.format(title)) for title in num_completed_tasks]
