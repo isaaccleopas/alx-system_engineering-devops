abae9ecf82e300189ff3c5b625c5da2765d0f0ae
isaaccleopas <isaaccleopas@gmail.com>
2023-05-09 17:27:12 +0000

    task 1 corrected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,9 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(sudreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {"limit": 10}
+    params = {
+        "limit": 10
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
