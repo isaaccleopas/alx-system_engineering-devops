ad95398a8053ad6daee11ba9b1f70246d6031927
isaaccleopas <isaaccleopas@gmail.com>
2023-05-09 17:45:42 +0000

    task 100 corrected

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,34 +2,39 @@
 """Counts it."""
 import requests
 
-def count_words(subreddit, word_list, count={}):
+
+def count_words(subreddit, word_list, after="", count={}):
     """
     Returns a list containing the titles of all hot articles for a
     given subreddit. If no results are found for the given subreddit,
     the function should return None.
     """
-    if not word_list:
-        sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_count:
-            print("{}: {}".format(word, count))
-        return
-
-    word = word_list[0].lower()
-    word_list = word_list[1:]
-
-    headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": "100", "after": after}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
     if response.status_code != 200:
+        return None
+    data = response.json().get("data")
+    if not data:
+        return None
+    children = data.get("children")
+    if not children:
+        return None
+    for child in children:
+        title = child.get("data").get("title")
+        if not title:
+            continue
+        words = title.lower().split()
+        for word in words:
+            word = word.rstrip('.,?!_')
+            if word in word_list:
+                count[word] = count.get(word, 0) + 1
+    after = data.get("after")
+    if not after:
+        sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        for word, cnt in sorted_count:
+            print("{}: {}".format(word, cnt))
         return
-
-    data = response.json()
-
-    for child in data['data']['children']:
-        title = child['data']['title'].lower()
-        occurrences = title.count(word)
-        if occurrences > 0:
-            count[word] = count.get(word, 0) + occurrences
-
-    count_words(subreddit, word_list, count)
+    count_words(subreddit, word_list, after, count)
