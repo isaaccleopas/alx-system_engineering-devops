d7a71a71627a615b28a5688e396fa6eef8f1bc64
isaaccleopas <isaaccleopas@gmail.com>
2023-05-09 17:02:55 +0000

    ad& task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,17 +3,11 @@
 import requests
 
 def top_ten(subreddit):
-    """
-    Print the titles of the 10 hottest
-    posts on a given subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()['data']['children']
-
         for post in data[:10]:
             print(post['data']['title'])
     else:
