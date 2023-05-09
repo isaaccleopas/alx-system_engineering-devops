2505b5720204f8072d6e25f08ab86c388d751067
isaaccleopas <isaaccleopas@gmail.com>
2023-05-09 15:00:42 +0000

    correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'limit': 100, 'after': after}
@@ -13,8 +15,10 @@ def recurse(subreddit, hot_list=[], after=None):
         data = response.json()['data']
         children = data['children']
         after = data['after']
+
         for child in children:
             hot_list.append(child['data']['title'])
+
         if after is not None:
             recurse(subreddit, hot_list, after)
         return hot_list
