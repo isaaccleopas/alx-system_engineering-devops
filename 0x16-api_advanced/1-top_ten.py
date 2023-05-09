c5f81ac2fc5a4462cbec436f5d1baa5a9924b81f
isaaccleopas <isaaccleopas@gmail.com>
2023-05-09 16:39:12 +0000

    advanced corrected & task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,8 +9,7 @@ def top_ten(subreddit):
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()['data']['children']
