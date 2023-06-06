abed708cfe91df7dc58034e10a23cb2e42ac4146
isaaccleopas <isaaccleopas@gmail.com>
2023-06-06 13:05:21 +0000

    tasks 2 and 100 added

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Counts it."""
+import requests
+
+
+def count_words(subreddit, word_list, after="", count={}):
+    """
+    Returns a list containing the titles of all hot articles for a
+    given subreddit. If no results are found for the given subreddit,
+    the function should return None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": "100", "after": after}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    data = response.json().get("data")
+    if not data:
+        return None
+    children = data.get("children")
+    if not children:
+        return None
+    for child in children:
+        title = child.get("data").get("title")
+        if not title:
+            continue
+        words = title.lower().split()
+        for word in words:
+            word = word.rstrip('.,?!_')
+            if word in word_list:
+                count[word] = count.get(word, 0) + 1
+    after = data.get("after")
+    if not after:
+        sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        for word, cnt in sorted_count:
+            print("{}: {}".format(word, cnt))
+        return
+    count_words(subreddit, word_list, after, count)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/python3o
 """Recursively queries Reddit for titles of hot articles for a subreddit."""
 import requests
 
+
 def recurse(subreddit, hot_lists=[]):
     """lists containing the titles of all hot articles for a subreddit"""
+    def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
+    if response.status_code == 200:
+        data = response.json()['data']
+        children = data['children']
+        after = data['after']
+
+        for child in children:
+            hot_list.append(child['data']['title'])
+
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
+        return None
