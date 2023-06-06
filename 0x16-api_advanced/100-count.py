8764d565e036554df196ac37de5c519633398c1e
isaaccleopas <isaaccleopas@gmail.com>
2023-06-06 15:35:48 +0000

    task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,38 +3,39 @@
 import requests
 
 
-def count_words(subreddit, word_list, after="", count={}):
+def count_words(subreddit, word_list, after=None, count_dict=None):
     """
     Returns a list containing the titles of all hot articles for a
     given subreddit. If no results are found for the given subreddit,
     the function should return None.
     """
+    if count_dict is None:
+        count_dict = {}
+
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": "100", "after": after}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    params = {"limit": 100, "after": after}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code != 200:
-        return None
-    data = response.json().get("data")
-    if not data:
-        return None
-    children = data.get("children")
-    if not children:
-        return None
-    for child in children:
-        title = child.get("data").get("title")
-        if not title:
-            continue
-        words = title.lower().split()
-        for word in words:
-            word = word.rstrip('.,?!_')
-            if word in word_list:
-                count[word] = count.get(word, 0) + 1
-    after = data.get("after")
-    if not after:
-        sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
-        for word, cnt in sorted_count:
-            print("{}: {}".format(word, cnt))
         return
-    count_words(subreddit, word_list, after, count)
+
+    data = response.json()
+    posts = data["data"]["children"]
+    for post in posts:
+        title = post["data"]["title"].lower()
+        for word in word_list:
+            word = word.lower()
+            if word in title:
+                count_dict[word] = count_dict.get(word, 0) + 1
+
+    next_page = data["data"]["after"]
+    if next_page is not None:
+        return count_words(subreddit, word_list, after=next_page,
+                           count_dict=count_dict)
+
+    sorted_counts = sorted(count_dict.items(),
+                           key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print("{}: {}".format(word, count))
