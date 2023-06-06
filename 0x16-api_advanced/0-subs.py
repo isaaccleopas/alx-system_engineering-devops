a3cabeda6d8b0fdbc53febdf952f0021b937f570
isaaccleopas <isaaccleopas@gmail.com>
2023-06-06 12:50:59 +0000

    tasks 1 and 2 corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,15 @@
 """This queries the subscribers on Reddit of subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Function returns the number of subscribers of subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent':'Mozilla/5.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-
-        return data['data']['subscribers']
+        return data["data"]["subscribers"]
     else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,18 +2,19 @@
 """Query Reddit for hot post of a subreddit."""
 import requests
 
+
 def top_ten(subreddit):
     """This function prints top 10 post of a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent':'Mozilla/5.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
+    params = {"limit": 10}
     if response.status_code == 200:
         data = response.json()
-        posts = data['data']['children']
-
+        posts = data["data"]["children"]
         for post in posts[:10]:
-            title = post['data']['title']
+            title = post["data"]["title"]
             print(title)
     else:
         print("None")
