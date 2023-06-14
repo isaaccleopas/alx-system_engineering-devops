8f67418752d8eda96414f9ed5c74856f1f8771e7
isaaccleopas <isaaccleopas@gmail.com>
2023-06-14 13:43:52 +0000

    ulimit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+# Update the ULIMIT value in /etc/default/nginx
+exec { 'update-nginx-ulimit':
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  path    => '/bin:/usr/bin',
+  onlyif  => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
+}
+
+# Restart Nginx after updating the ULIMIT
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['update-nginx-ulimit'],
+}
